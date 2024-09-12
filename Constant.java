29350b84a0e946e83d63ac6ecfb74013ed6c32d8
Kiran Choudhary <kiranchoudhary9180@gmail.com>
2024-09-12 06:48:26 +0530

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,17 @@
+/***
+ * This class is used for connecting the Methods class for taking the inputs from user.
+ */
+
+package Assignment;
+
+public final class Constant {
+	
+	final String COUNT_PALINDROME_METHOD = "1 - Count Palindrome";
+	final String NTH_FIBONACII_METHOD = "2  - Nth Fibonacii";
+	final String SNAKE_TO_CAMEL_METHOD = "3  - Snake to Camel";
+	final String COUNTS_CONSTANT_METHOD = "4  - Counts Constant";
+	final String BINARY_TO_DECIMAL_METHOD = "5- Binary to Decimal";
+	final String ENTER_INPUT = "Enter the method which you want to perform : ";
+	final String ENTER_STRING = "Enter the String : ";
+
+}
