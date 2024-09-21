fa71f76e53ea307f5a2da8229cc4f83b7790c9f5
Kiran Choudhary <kiranchoudhary9180@gmail.com>
2024-09-21 16:32:10 +0530

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,67 @@
+/***
+ * This class is used for connecting the Cricket_Match class for taking the inputs from user.
+ */
+
+
+package Assignment_5;
+
+public class Constant {
+	
+	final String WELCOME = "\u001B[1m\u001B[31m\t\tWELCOME\u001B[0m";
+	final String USER_CHOICE = "What you want to do : ";
+	final String CREATE_TEAM = "1. Create Team";
+	final String VIEW_TEAM = "2. View Team";
+	final String START_MATCH = "3. Start Match";
+	final String CHOICE = "Enter your Choice : ";
+	final String PERFORM = "What you want to perform : ";
+	final String CREATE_A_TEAM = "1. Create a Team";
+	final String EXIT = "2. Exit";
+	final String INVALID_CHOICE ="Invalid choice! Please try again.";
+	final String NOT_ENOUGH_TEAM = "Not enough teams to play a match. Please create more teams.";
+	final String INVALID_INPUT = "Invalid Input! \nPlease select the valid number.";
+	final String REPEAT_GAME = "Do you want to continue the game? (Y/N)";
+	final String GAME_ENDED = "Game Ended!!";
+	final String TEAM_SIZE = "How many teams do you want to create: ";
+	final String DUPLICATE_TEAM = "This team name already exists! Please enter a different name.";
+	final String PLAYER_SIZE = "How many players do you want to add: ";
+	final String INVALID_TEAM_PLAYER_SIZE = "\"Atleast 11 players are required for a match.";
+	final String PLAYER_NAME = "Enter the Name of Player: ";
+	final String PLAYER_SPECIFICATION = "What are the specifications of Player:";
+	final String BATSMAN = "1. Batsman";
+	final String BOWLER ="2. Bowler";
+	final String MORE_TEAM_CREATE_CHOICE = "Would you like to create more teams or Exit?";
+	final String CREATE_MORE_TEAM = "1. Create More Teams";
+	final String NO_TEAMS = "No teams are created yet!";
+	final String CREATED_TEAMS = "Created Teams : ";
+	final String PLAY_MATCH = "\u001B[1m\u001B[31m\t\tLet's play a Match!\u001B[0m";
+	final String TEAMS_OF_MATCH = "In which teams you want a match";
+	final String FIRST_TEAM = "Select First Team : ";
+	final String SECOND_TEAM = "Select Second Team : ";
+	final String ALREADY_SELECTED_TEAM = "Already selected!! Please try again.";	
+	final String PLAYERNAME_AND_PLAYERSPECIFICATION = "Players and their specifications:";
+	final String SELECTED_PLAYER_TEAM1 = "Select the 11 Players fron team-1 for Match : ";
+	final String SELECTED_PLAYER_TEAM2 = "Select the 11 Players from team-2 for Match : ";
+	final String TOTAL_OVERS = "How many overs you want to play?";
+	final String TOSE = "\u001B[1m\u001B[31m\t\tLet's Tose!!\u001B[0m";
+	final String HEAD = "1. Head";
+	final String TAIL = "2. Tail";
+	final String TOSE_SELCTION = "What first team want to choose : ";
+	final String TOSE_WIN_TEAM1 = "\u001B[1m\u001B[31m\t\tTeam-1 win the toss.\nNow team-2 start Batting.!!\u001B[0m";
+	final String TEAM_PLAY = "\u001B[1m\u001B[31m\t\tNow Next Team is Ready to Play the Match!!\u001B[0m";
+	final String TOSE_WIN_TEAM2 = "\u001B[1m\u001B[31m\t\tTeam-2 win the toss.\nNow team-2 start Batting.!!\u001B[0m";
+	final String FINAL_SCORE = "Final Score:";
+	final String MATCH_TIE = "It's a Tie!";
+	final String MORE_MATCH = "Do you want to play one more Match? (Y / N)";
+	final String THANKS_FOR_MATCH = "\u001B[1m\u001B[31m\t\tThe Match is over!!\nThank You for Playing. \u001B[0m";
+	final String INVALID_PLAYER_NUMBER = "Invalid player number. Try again.";
+	final String SELECTED_PLAYER = "Player already selected. Try another one.";
+	final String BALL_TYPE = "What kind of ball is : ";
+	final String RUN = "1. Run";
+	final String WIDE = "2. Wide";
+	final String NO_BALL = "3. No ball";
+	final String WICKET = "4. Wicket";
+	final String RUN_TAKEN = "How many runs did the player take : ";
+	final String BATTING_COMPLETED = "All players from Team have batted.";
+	final String PLAYERS_RUN = "\nFinal Runs of Players:";
+	
+}
